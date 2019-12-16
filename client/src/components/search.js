7a1dd2407efcf03f585095e381709f70706b6e94
Steve Renner <che1111@hotmail.com>
2019-12-16 13:40:47 -0800

    Added GPS Locator

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -4,14 +4,16 @@ import { connect } from "react-redux"
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import GpsFixedIcon from '@material-ui/icons/GpsFixed';
+import { InputAdornment } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     container: {
         display: 'flex',
         flexWrap: 'wrap',
         flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: '10%'
+        alignItems: 'center'
+        
        
     },
     textField: {
@@ -41,7 +43,7 @@ const useStyles = makeStyles(theme => ({
 
     return (
         
-    <div className="business-search" style={{ height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alighItems: 'center', backgroundSize: 'cover', backgroundImage: 'url(https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9)' }}>
+    <div className="business-search" style={{ height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundSize: 'cover', backgroundImage: 'url(https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9)' }}>
         {/* <div class="mdc-text-field mdc-text-field--outlined">
         <input type="text" id="tf-outlined" class="mdc-text-field__input"></input>
         <div class="mdc-notched-outline"></div>
@@ -66,9 +68,24 @@ const useStyles = makeStyles(theme => ({
                 variant="outlined"
                 margin="normal"
                 type="text"
-                className={classes.textField}
+                className={`${classes.textField} `}
                 placeholder="City or State"
-            />
+            //     endAdornment={<InputAdornment position="end">
+            //     <GpsFixedIcon
+            //       aria-label="locator-icon"
+            //     //   onClick={handleClickShowPassword}
+            //     //   onMouseDown={handleMouseDownPassword}
+            //       edge="end"
+            //     >
+            //       {/* {values.showPassword ? <Visibility /> : <VisibilityOff />} */}
+            //     </GpsFixedIcon>
+            //   </InputAdornment>
+            
+                InputProps={{
+                endAdornment: <InputAdornment position="end"><GpsFixedIcon/></InputAdornment>,
+                }}
+            >
+            </TextField>
             <Button className ={classes.button} variant="outlined" color="blue" type="submit">Submit</Button>
         </form>
     </div>
