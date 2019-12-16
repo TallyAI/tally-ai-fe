32b3723e22d78b91a20073a5b58c8dfa4263daa8
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-12-16 14:02:49 -0800

    added phone, location, and img src url to result component

--- client/src/components/search/result.js
+++ client/src/components/search/result.js
@@ -3,13 +3,21 @@ import StarRatings from "react-star-ratings";
 
 const Result = ({ data, postBusiness }) => {
   return (
-    <div className="result">
-      <img />
+    <div className="result" onClick={postBusiness(data.url, data.id)}>
+      <img src={data.image_url} />
       <div className="result-text">
         <h3>{data.name}</h3>
-        <StarRatings 
-          rating={data}
+        <StarRatings
+          rating={data.rating}
+          starRatedColor="blue"
+          numberOfStars={5}
+          name="rating"
         />
+        <p>{data.phone}</p>
+        <p>{data.location.address1}</p>
+        <p>
+          {data.location.city}, {data.location.state} {data.location.zip_code}
+        </p>
       </div>
     </div>
   );
