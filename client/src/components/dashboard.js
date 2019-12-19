795612e1203f23dfc7174e3f14db77a1fb0788f3
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-12-19 15:12:47 -0800

    fixed dashboard bug - DS words now render in columnsga Co-authored-by: Daniel Firpo <trtld2@hotmail.com> Co-authored-by: Steve Renner <steverenner1@gmail.com> Co-authored-by: David Downes <davidgatrmacaulay@gmail.com> Co-authored-by: Ebi Baiden <ebaiden@techie.com>

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,52 +1,71 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import { Grid } from "@material-ui/core";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-      display: 'flex',
-      paddingTop: '5%',
-      flexDirection: 'column'
-    },
-    paper: {
-      padding: theme.spacing(1),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }));
-  
-  function DashboardGrid(props) {
-    const classes = useStyles();
-  
-    function FormRow() {
-      return (
-        <React.Fragment>
-          <Grid item xs={4} style={{display:"flex", flexDirection:"column"}}>
-            <Paper className={classes.paper}>item</Paper>
-          </Grid>
-          <Grid item xs={4} style={{display:"flex", flexDirection:"column"}}>
-            <Paper className={classes.paper}>item</Paper>
-          </Grid>
-        </React.Fragment>
-      );
-    }
-  
+  root: {
+    flexGrow: 1,
+    display: "flex",
+    paddingTop: "5%",
+    flexDirection: "column"
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
+
+function DashboardGrid(props) {
+  const classes = useStyles();
+
+  function FormRow() {
     return (
-      <div className={classes.root} >
+      <React.Fragment>
+        <Grid item xs={4} style={{ display: "flex", flexDirection: "column" }}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4} style={{ display: "flex", flexDirection: "column" }}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+  console.log("props.words in dashboard: ", props.words);
+
+  if (props.isFetching) {
+    return <h3>Loading analytics...</h3>;
+  } else {
+    return (
+      <div className={classes.root}>
         <Grid container spacing={1}>
-            {props.words.positive.map(word => {
-                return (
-                    <Grid item xs={4} style={{display:"flex", flexDirection:"column"}}>
-            <Paper className={classes.paper}>{word.term}</Paper>
-          </Grid>
-                )
-            })}
-          <Grid container item xs={6} spacing={3}>
+          {props.words.positive.map(word => {
+            return (
+              <Grid
+                item
+                xs={6}
+                style={{ display: "flex", flexDirection: "column" }}
+              >
+                <Paper className={classes.paper}>{word.term}</Paper>
+              </Grid>
+            );
+          })}
+          {props.words.negative.map(word => {
+            return (
+              <Grid
+                item
+                xs={6}
+                style={{ display: "flex", flexDirection: "column" }}
+              >
+                <Paper className={classes.paper}>{word.term}</Paper>
+              </Grid>
+            );
+          })}
+          {/* <Grid container item xs={6} spacing={3}>
             <FormRow />
           </Grid>
           <Grid container item xs={6} spacing={3}>
@@ -75,14 +94,15 @@ const useStyles = makeStyles(theme => ({
           </Grid>
           <Grid container item xs={6} spacing={3}>
             <FormRow />
-          </Grid>
+          </Grid> */}
         </Grid>
       </div>
     );
   }
+}
+const mapStateToProps = state => ({
+  words: state.keyWords.data,
+  isFetching: state.keyWords.isFetching
+});
 
-  const mapStateToProps = state => ({
-      words: state.keyWords.data
-    });
-
-  export default connect(mapStateToProps)(DashboardGrid);
+export default connect(mapStateToProps)(DashboardGrid);
