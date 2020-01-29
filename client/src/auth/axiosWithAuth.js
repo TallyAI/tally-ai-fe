05345faf522a460294e35b17a10f06d4ce499e3c
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-01-29 15:52:01 -0800

    updated base url for BE

--- client/src/auth/axiosWithAuth.js
+++ client/src/auth/axiosWithAuth.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
 
-    if(token === "undefined") {
+    if (token === "undefined") {
         alert("WARNING. Making requests that might require auth while not logged in.");
     }
 
     return axios.create({
-        baseURL: "https://tally-ai.herokuapp.com/api/",
+        baseURL: "http://tallyai.us-east-1.elasticbeanstalk.com/api/",
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `${token}`,
